6351dacf442809ce51fdb8caac85f6fba43632ca
vjcetwebadmin <webadmin@vjcet.org>
2019-05-24 14:46:41 +0530

    Banner news spelling correction

--- src/HomePageContainers/CarouselTwo.js
+++ src/HomePageContainers/CarouselTwo.js
@@ -15,15 +15,15 @@ class CarouselTwo extends Component {
     },
     {
       instanceID: this.props.data.instanceID + "CustomOne",
-      title: "Semester 6 Regular an Supplementry Exams commence on May 27"
+      title: "Semester 6 Regular an Supplementary Exams commence on May 27"
     },
     {
       instanceID: this.props.data.instanceID + "CustomOne",
-      title: "Semester 4 Regular and Supplementry Exams commence on June 3"
+      title: "Semester 4 Regular and Supplementary Exams commence on June 3"
     },
     {
       instanceID: this.props.data.instanceID + "CustomOne",
-      title: "Semester 2 Regular and Supplementry Exams commenece on June 13"
+      title: "Semester 2 Regular and Supplementary Exams commenece on June 13"
     },
     
   ];
