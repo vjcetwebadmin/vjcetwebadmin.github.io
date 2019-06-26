e4542e97aa045d50fdecd8da014f4e41b3e97260
vjcetwebadmin <webadmin@vjcet.org>
2019-06-26 17:06:48 +0530

    Facilities added

--- src/Pages/Blockchain.js
+++ src/Pages/Blockchain.js
@@ -2,7 +2,7 @@ import React from "react";
 import HeadingTwo from "../Components/Texts/HeadingTwo";
 import ImageOne from  "../Components/Images/ImageOne";
 import BodyOneJustified from "../Components/Texts/BodyOneJustified";
-import "../FacilitiesPageContainers/styles.css";
+import "../Facilities/styles.css";
 
 import Header from "../Components/Header&Footer/Header";
 import Footer from "../Components/Header&Footer/Footer";
