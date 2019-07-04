b59c9a9435233238cf8ebbbfeb7818c1c1db71fc
vjcetwebadmin <webadmin@vjcet.org>
2019-07-04 22:05:24 +0530

    carousalfour css updated, driver contacts updated

--- src/FacilitiesPageContainers/Transportation/transportation.js
+++ src/FacilitiesPageContainers/Transportation/transportation.js
@@ -77,64 +77,64 @@ class transportation extends React.Component {
                         <HeadingTwo data={this.BusserviceTitle} className="Heading" />
                         <table id="tableformat">
                             <tr>
-                                <th>Route</th> <th>Starting Time</th><th>Capacity</th>                              
+                                <th>Route</th> <th>Starting Time</th><th>Contact</th>                              
                             </tr>
                             <tr>
-                               <td>Neriamangalam</td> <td>7.35am</td><td>54</td>
+                               <td>Neriamangalam</td> <td>7.35am</td><td>Mr. Mathew George-9048412370</td>
                             </tr>
                             <tr>
-                               <td>Palai</td> <td>7.30am</td><td>33</td>
+                               <td>Palai</td> <td>7.30am</td><td>Mr. V M Varkey-9744923792</td>
                             </tr>
                             <tr>
-                               <td>Thommankuthu</td> <td>7.35am</td><td>47</td>
+                               <td>Thommankuthu</td> <td>7.35am</td><td>Mr. Rajesh Manuel-9446802589</td>
                             </tr>
                             <tr>
-                               <td>Kuravilangadu</td> <td>7.15am</td><td>47</td>
+                               <td>Kuravilangadu</td> <td>7.15am</td><td>Mr. K V John-9946309697</td>
                             </tr>
                             <tr>
-                               <td>Oonnukal</td> <td>7.40am</td><td>47</td>
+                               <td>Oonnukal</td> <td>7.40am</td><td>Mr. E V Mathai-9744503225</td>
                             </tr>
                             <tr>
-                               <td>Puthencruz </td> <td>7.45am</td><td>47</td>
+                               <td>Puthencruz </td> <td>7.45am</td><td>Mr. M A Mathew-9400861748</td>
                             </tr>
                             <tr>
-                               <td>Moolamattom</td> <td>7.35am</td><td>47</td>
+                               <td>Moolamattom</td> <td>7.35am</td><td>Mr. P S Jayan-9747944537</td>
                             </tr>
                             <tr>
-                               <td>Thalayolaparambu</td> <td>7.20am</td><td>47</td>
+                               <td>Thalayolaparambu</td> <td>7.20am</td><td>Mr. Sajeesh Valsalan-9847330512</td>
                             </tr>
                             <tr>
-                               <td>Kakkanadu</td> <td>7.15am</td><td>47</td>
+                               <td>Kakkanadu</td> <td>7.15am</td><td>Mr. Roy John-9656098480</td>
                             </tr>
                             <tr>
-                               <td>Nedungapra</td> <td>7.45am</td><td>47</td>
+                               <td>Nedungapra</td> <td>7.45am</td><td>Mr. K V George-9400428912</td>
                             </tr>
                             <tr>
-                               <td>Karimannoor</td> <td>7.30am</td><td>47</td>
+                               <td>Karimannoor</td> <td>7.30am</td><td>Mr. M J Johny-9544297112</td>
                             </tr>
                             <tr>
-                               <td>Perumbavoor</td> <td>7.55am</td><td>47</td>
+                               <td>Perumbavoor</td> <td>7.55am</td><td>Mr. Vinodkumar-9447219100</td>
                             </tr>
                             <tr>
-                               <td>Kuruppampady</td> <td>7.35am</td><td>47</td>
+                               <td>Kuruppampady</td> <td>7.35am</td><td>Mr. C T Valsan-9747911051</td>
                             </tr>
                             <tr>
-                               <td>Vyttila</td> <td>7.15am</td><td>47</td>
+                               <td>Vyttila</td> <td>7.15am</td><td>Mr. K A Jose-9846477834</td>
                             </tr>
                             <tr>
-                               <td>Aluva</td> <td>7.15am</td><td>47</td>
+                               <td>Aluva</td> <td>7.15am</td><td>Mr. Ponnappan C-9946797133</td>
                             </tr>
                             <tr>
-                               <td>Thriruvankulam</td> <td>7.35am</td><td>47</td>
+                               <td>Thriruvankulam</td> <td>7.35am</td><td>Mr. Paul Manuel-9847998339</td>
                             </tr>
                             <tr>
-                               <td>Udumbannoor</td> <td>7.25am</td><td>47</td>
+                               <td>Udumbannoor</td> <td>7.25am</td><td>Mr. Sasi C K-9495194429</td>
                             </tr>
                             <tr>
-                               <td>Angamaly</td> <td>7.25am</td><td>47</td>
+                               <td>Angamaly</td> <td>7.25am</td><td>Mr. V U Jose-9446888528</td>
                             </tr>
                             <tr>
-                               <td>Kuttampuzha</td> <td>7.25am</td><td>47</td>
+                               <td>Kuttampuzha</td> <td>7.25am</td><td>Mr. Mr. Jose-9847487212</td>
                             </tr>
              </table>
              <HeadingTwo data={this.RoutemapTitle} className="Heading" />
