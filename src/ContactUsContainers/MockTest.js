bdc3bb311ccc67cf54e191761177307149aee8e7
erpadmin <webadmin@vjcet.org>
2021-04-26 15:14:18 +0530

    updt four

--- src/ContactUsContainers/MockTest.js
+++ src/ContactUsContainers/MockTest.js
@@ -35,39 +35,15 @@ class mocktest extends React.Component {
     return (
       <React.Fragment>
         <Header data={this.HomePageHeader} openMenuFunction={this.openMenu} />
-        <div classNameName="bgg" id="contactUsListing">
+        {/*<div classNameName="bgg" id="contactUsListing"> two column design
           <div className="root">
             <div>
-              <div className="child">
-                <HeadingOne data={this.ReachUsHeading} />
+    <div className="child">*/}
+    <div className="contain">
+            <HeadingOne data={this.ReachUsHeading} className="Heading" />
                 <h3 className="add"> Level 1</h3>
                 <p><a href=" https://forms.gle/fuoRWBMs1gQnNASj7" target="_blank" rel="noopener noreferrer"><b>Paper 1 - Physics & Chemistry</b></a></p>
                 <p><a href="https://forms.gle/Ez6tRswMSidn4nYt5" target="_blank" rel="noopener noreferrer"><b>Paper 2 - Mathematics</b></a></p>               
-                {/*<h3 className="Reach1">
-                  Viswajyothi College of Engineering & Technology Vazhakulam,
-                  <br />
-                  Muvattupuzha,
-                  <br />
-                  Ernakulam,Kerala,
-                  <br />
-                  India-686670
-                </h3>*/}
-              </div>
-             </div>
-            {/*<div className="child">
-              {/* <h1 className="Heading">Enquiry</h1> 
-              <HeadingOne data={this.EnquiryHeading} />
-              <h3 className="Reach1">
-              <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d125752.05890978406!2d76.5889803!3d9.9545934!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b07dd2f387bbce7%3A0x33c9780276b797b6!2sViswajyothi+College+of+Engineering+and+Technology!5e0!3m2!1sen!2sin!4v1555349832912!5m2!1sen!2sin"
-                  width="450"
-                  height="450"
-                  frameborder="0"
-                  title="google-map"
-                />
-              </h3>
-            </div>*/}
-          </div>
         </div>
         <Footer data={this.HomePageFooter} />
         {this.state.showMenu ? (
