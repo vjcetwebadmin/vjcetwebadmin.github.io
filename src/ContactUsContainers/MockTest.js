081bcc78d89142654bb55ace70ae26fa46976526
erpadmin <webadmin@vjcet.org>
2021-05-03 14:59:16 +0530

    mock updt 030520201

--- src/ContactUsContainers/MockTest.js
+++ src/ContactUsContainers/MockTest.js
@@ -41,9 +41,23 @@ class mocktest extends React.Component {
     <div className="child">*/}
     <div className="contain">
             <HeadingOne data={this.ReachUsHeading} className="Heading" />
-                <h3 className="add"> Level 1</h3>
+              <table width="60%"><tr><td colSpan="2"><h3><b>Click the link to attend the Mock Test. Your Score will be informed via SMS.</b></h3></td></tr>
+              <tr><td rowspan="2"><h3> Level 1</h3></td><td><a href=" https://forms.gle/fuoRWBMs1gQnNASj7" target="_blank" rel="noopener noreferrer"><b>Paper 1 - Physics & Chemistry</b></a></td></tr>
+              <tr><td><a href="https://forms.gle/Ez6tRswMSidn4nYt5" target="_blank" rel="noopener noreferrer"><b>Paper 2 - Mathematics</b></a></td></tr>
+
+
+              <tr><td><h3> Level 2</h3></td><td>On second week of May 2021</td></tr>
+
+              <tr><td><h3> Level 3</h3></td><td>On third week of May 2021</td></tr>
+
+              <tr><td><h3> Level 4</h3></td><td>On fourth week of May 2021</td></tr>
+              
+              </table>
+
+
+               {/* <h3 className="add"> Level 1</h3>
                 <p><a href=" https://forms.gle/fuoRWBMs1gQnNASj7" target="_blank" rel="noopener noreferrer"><b>Paper 1 - Physics & Chemistry</b></a></p>
-                <p><a href="https://forms.gle/Ez6tRswMSidn4nYt5" target="_blank" rel="noopener noreferrer"><b>Paper 2 - Mathematics</b></a></p>               
+  <p><a href="https://forms.gle/Ez6tRswMSidn4nYt5" target="_blank" rel="noopener noreferrer"><b>Paper 2 - Mathematics</b></a></p>               */}
         </div>
         <Footer data={this.HomePageFooter} />
         {this.state.showMenu ? (
