51128d8dca6f69bade10c8d585a261f8d61aba0c
erpadmin <webadmin@vjcet.org>
2021-05-18 16:20:34 +0530

    mba news3

--- src/Pages/NewsContainers/Newsthree.js
+++ src/Pages/NewsContainers/Newsthree.js
@@ -78,7 +78,7 @@ class Newsthree extends React.Component {
                   {/* <HeadingOne data={this.NewsTitle} https://vjcet.org/>*/}
                    
                    <HeadingTwo data={this.NewsTitle1} />
-                  <iframe src="https://vjcet.org/downloads/news/FacRec.pdf#zoom=250" width="85%" height="600" title="Campus Map" align="center"></iframe>
+                  <iframe src="https://vjcet.org/downloads/news/FacRec.pdf#zoom=115" width="90%" height="800" title="Faculty Recruitment" align="center"></iframe>
                     
                 </div>
                 <Footer data={this.HomePageFooter} />
