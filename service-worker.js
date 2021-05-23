e90fb4bc841c36da4b74c3d3106123e5dd4f2ce1
erpadmin <webadmin@vjcet.org>
2021-05-24 00:06:48 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.049eb212c6b65009f279b9467fc1d4c4.js"
+  "/precache-manifest.9208a146db23cbb903e72dec0401f028.js"
 );
 
 self.addEventListener('message', (event) => {
